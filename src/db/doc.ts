f85b244554e30ff1923ccd1db9ede3fb90cfe39e
Alexander Shilov <aleksandr.schilow2012@gmail.com>
2025-08-04 13:02:55 +0300

    Add IndexedDB persistence provider

--- src/db/doc.ts
+++ src/db/doc.ts
@@ -1,10 +1,19 @@
 import * as Y from "yjs";
 import { WebsocketProvider } from "y-websocket";
+import { IndexeddbPersistence } from "y-indexeddb";
 
 export const doc = new Y.Doc();
+
+const DOC_NAME = "test3";
 
 const wsProvider = new WebsocketProvider("ws://localhost:1234", "test3", doc);
 
 wsProvider.on("status", (event) => {
-  console.log("status", event.status);
+  console.log("wsProvider status: " + event.status);
+});
+
+const idbProvider = new IndexeddbPersistence(DOC_NAME, doc);
+
+idbProvider.on("synced", () => {
+  console.log("idbProvider synced");
 });
