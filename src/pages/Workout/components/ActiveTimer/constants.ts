aa3fe0e0a9d0f58ab7b71eef260ec8d0d38af65d
Alexander Shilov <aleksandr.schilow2012@gmail.com>
2025-10-26 21:35:46 +0300

    Timer sound experiment

--- src/pages/Workout/components/ActiveTimer/constants.ts
+++ src/pages/Workout/components/ActiveTimer/constants.ts
@@ -1,3 +1,17 @@
 import { atom } from "jotai";
+import { default as SoundUrl } from "./sound_with_silence.mp3";
 
 export const TIMER_DEADLINE_ATOM = atom<number | null>(null);
+
+export const AUDIO_ELEMENT = (() => {
+  const source = document.createElement("source");
+  source.src = SoundUrl;
+  source.type = "audio/mpeg";
+
+  const audio = document.createElement("audio");
+  audio.appendChild(source);
+
+  document.body.appendChild(audio);
+
+  return audio;
+})();
--- src/pages/Workout/components/ActiveTimer/hooks.ts
+++ src/pages/Workout/components/ActiveTimer/hooks.ts
@@ -1,11 +1,12 @@
 import { useAtom } from "jotai";
-import { TIMER_DEADLINE_ATOM } from "./constants.ts";
+import { AUDIO_ELEMENT, TIMER_DEADLINE_ATOM } from "./constants.ts";
 
 export function useActiveTimer() {
   const [, setDeadline] = useAtom(TIMER_DEADLINE_ATOM);
   return {
     startTimer: (timeSeconds: number | undefined) => {
       setDeadline(timeSeconds ? Date.now() + timeSeconds * 1000 : null);
+      AUDIO_ELEMENT.play();
     },
   };
 }
